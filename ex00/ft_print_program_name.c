fc6975300fed9e105a8476cdb27a313c8c32655a
banregion <banregion@gmail.com>
2025-04-28 15:50:21 +0300

    Update ft_print_program_name.c

--- ex00/ft_print_program_name.c
+++ ex00/ft_print_program_name.c
@@ -1,7 +1,33 @@
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: byildiza < byildiza@student.42kocaeli.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/26 22:29:24 by byildiza          #+#    #+#             */
+/*   Updated: 2025/04/28 11:19:16 by byildiza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int main(int argc, char *argv[]) 
+#include <unistd.h>
+
+int	ft_strlen(char *str)
 {
-    printf("%s\n", argv[0]);
-    return 0;
+	int	len;
+
+	len = 0;
+	while (str[len] != '\0')
+		len++;
+	return (len);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc > 0)
+		write(1, argv[0], ft_strlen(argv[0]));
+	else
+		return (1);
+	write(1, "\n", 1);
+	return (0);
 }
