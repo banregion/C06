97d7826aed0cd8456ed5e3ef831f1e262740b7de
banregion <banregion@gmail.com>
2025-04-28 15:46:33 +0300

    Update ft_sort_params.c

--- ex03/ft_sort_params.c
+++ ex03/ft_sort_params.c
@@ -55,3 +55,68 @@ int main(int argc,char *argv[])
     return 0;
     
 }
+ /*#include <unistd.h>
+
+void	put_char(char c)
+{
+	write(1, &c, 1);
+}
+
+int	ft_strcmp(char *a, char *b)
+{
+	while (*a && *b)
+	{
+		if (*a != *b)
+			return (*a - *b);
+		a++;
+		b++;
+	}
+	return (*a - *b);
+}
+
+void	swap(char **a, char **b)
+{
+	char	*tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+void	sort_args(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	while (i < argc - 1)
+	{
+		j = i + 1;
+		while (j < argc)
+		{
+			if (ft_strcmp(argv[i], argv[j]) > 0)
+				swap(&argv[i], &argv[j]);
+			j++;
+		}
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int		i;
+	char	*p;
+
+	sort_args(argc, argv);
+	i = 1;
+	while (i < argc)
+	{
+		p = argv[i];
+		while (*p)
+			put_char(*p++);
+		put_char('\n');
+		i++;
+	}
+	return (0);
+}
+*/
