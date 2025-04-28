8dd7b5693a6fe70716e28640047641829605aba1
banregion <banregion@gmail.com>
2025-04-28 15:50:05 +0300

    Update ft_print_params.c

--- ex01/ft_print_params.c
+++ ex01/ft_print_params.c
@@ -1,10 +1,33 @@
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: byildiza < byildiza@student.42kocaeli.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/26 22:26:39 by byildiza          #+#    #+#             */
+/*   Updated: 2025/04/28 11:18:12 by byildiza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int main(int argc, char *argv[]) 
+#include <unistd.h>
+
+int	main(int argc, char **argv)
 {
-    argv[1] = "test1";
-    argv[2] = "test2";
-    argv[3] = "test3";
-    printf("%s\n%s\n%s\n", argv[1],argv[2],argv[3]);
-    return 0;
+	int	i;
+	int	len;
+
+	if (argc == 1)
+		return (0);
+	i = 1;
+	while (i < argc)
+	{
+		len = 0;
+		while (argv[i][len] != '\0')
+			len++;
+		write(1, argv[i], len);
+		write(1, "\n", 1);
+		i++;
+	}
+	return (0);
 }
